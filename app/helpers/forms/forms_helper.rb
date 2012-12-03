95ed8791353ace1b746adc9825578a56c2c1ce46
Sam Pohlenz <sam@sampohlenz.com>
2012-12-03 11:00:57 +1030

    Clean up admin field rendering

--- app/helpers/forms/forms_helper.rb
+++ app/helpers/forms/forms_helper.rb
@@ -1,4 +1,27 @@
 module Forms::FormsHelper
+  class FormBuilder < ActionView::Helpers::FormBuilder
+    def required_field_checkbox
+      @template.content_tag(:label, :class => "required") do
+        check_box(:required, :id => nil) + " This field is required"
+      end
+    end
+  end
+  
+  def form_field(field, options={}, &block)
+    builder = FormBuilder.new(options[:prefix], field, self, {}, block)
+    icon = options[:icon] || field.class.model_name.demodulize.underscore.dasherize
+    
+    content_tag(:div, :class => options[:class], :data => { :index => options[:index] }) do
+      concat builder.hidden_field(:type, :value => field.type.demodulize, :id => nil)
+      
+      concat content_tag(:div, image_tag("wheelhouse-forms/#{icon}.png", :alt => field.class.model_name.human, :title => field.class.model_name.human), :class => "drag")
+      
+      concat content_tag(:div, capture(builder, &block), :class => "drag-area")
+      
+      concat link_to("Delete", '#', :class => 'delete')
+    end
+  end
+  
   def render_fields(fields, prefix)
     fields.map_with_index do |field, index|
       render(:partial => field.class.partial, :object => field, :locals => { :index => index, :prefix => "#{prefix}[#{index}]", :id => "" })
@@ -9,18 +32,5 @@ module Forms::FormsHelper
     jquery_template_tag(id) do
       render :partial => klass.partial, :object => klass.new, :locals => { :index => "${index}", :prefix => "${prefix}", :id => "${id}" }
     end
-  end
-  
-  def basic_field(object, options={})
-    render :partial => "basic_field",
-           :object => object,
-           :locals => {
-                        :id => options[:id],
-                        :prefix => options[:prefix],
-                        :index => options[:index],
-                        :type => options[:type],
-                        :description => options[:description],
-                        :icon => options[:icon] || options[:type]
-                      }
   end
 end
