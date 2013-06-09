14759d9dc2ac773cd89ed52a5ef32caad152110e
Sam Pohlenz <sam@sampohlenz.com>
2013-06-09 15:29:25 +0930

    Add mark spam/not spam buttons to view submission page

--- app/assets/javascripts/wheelhouse-forms/submissions.js
+++ app/assets/javascripts/wheelhouse-forms/submissions.js
@@ -1,28 +1,56 @@
 $(function() {
 
+function updateSubmission(url, spam, callback) {
+  $.ajax({
+    type: 'PUT',
+    url: url,
+    data: { spam: spam },    
+    dataType: 'json',
+    success: function() {
+      Wheelhouse.Flash.message("Submission updated.");
+      callback();
+    }
+  });
+  
+  Wheelhouse.Flash.loading("Marking submission as spam.");
+}
+
 function clickHandler(spam) {
   return function() {
     var url = $(this).attr('href');
     var row = $(this).closest('tr');
-
-    $.ajax({
-      type: 'PUT',
-      url: url,
-      data: { spam: spam },    
-      dataType: 'json',
-      success: function() {
-        Wheelhouse.Flash.message("Submission updated.");
-        row.remove();
-      }
+    
+    updateSubmission(url, spam, function() {
+      row.remove();
     });
     
-    Wheelhouse.Flash.loading("Marking submission as spam.");
-
     return false;
   }
 }
 
 $('#submissions').on('click', 'a.mark-spam', clickHandler(true));
 $('#submissions').on('click', 'a.not-spam', clickHandler(false));
+
+$('button.mark-spam').click(function() {
+  var url = $(this).closest('form').attr('action');
+  
+  updateSubmission(url, true, function() {
+    $('button.mark-spam').hide();
+    $('button.not-spam').show();
+  });
+  
+  return false;
+});
+
+$('button.not-spam').click(function() {
+  var url = $(this).closest('form').attr('action');
+  
+  updateSubmission(url, false, function() {
+    $('button.not-spam').hide();
+    $('button.mark-spam').show();
+  });
+  
+  return false;
+});
 
 });
