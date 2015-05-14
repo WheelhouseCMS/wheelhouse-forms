8242303470412d11328548e6293be23f823e79eb
Sam Pohlenz <sam@sampohlenz.com>
2015-05-14 11:22:10 +0930

    Add config option to disable admin links in notification emails

--- lib/wheelhouse-forms.rb
+++ lib/wheelhouse-forms.rb
@@ -15,10 +15,14 @@ module Forms
   class Plugin < Wheelhouse::Plugin
     config.wheelhouse.forms = ActiveSupport::OrderedOptions.new
     
-    # Disable custom fields by default
+    # Disable custom fields by default.
     config.wheelhouse.forms.custom_fields = false
+
+    # Show 'view submission' link in notification emails.
+    # You may wish to disable this if you plan to reply directly to notification emails.
+    config.wheelhouse.forms.show_admin_link_in_notifications = true
     
-    # Set default spam filter
+    # Set default spam filter.
     config.wheelhouse.forms.spam_filter = defined?(Rakismet) ? AkismetFilter : NullFilter
     
     isolate_namespace Forms
