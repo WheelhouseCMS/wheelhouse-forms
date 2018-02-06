64bb2d22d9e8dcdbab99462f452a0b816638efb2
Sam Pohlenz <sam@sampohlenz.com>
2018-02-06 12:00:55 +1030

    Use string for submissions controller belongs_to option

--- app/controllers/forms/submissions_controller.rb
+++ app/controllers/forms/submissions_controller.rb
@@ -1,22 +1,22 @@
 require_dependency "forms/csv_exporter"
 
 class Forms::SubmissionsController < Wheelhouse::ResourceController
-  belongs_to :form, :class_name => Forms::Form
+  belongs_to :form, :class_name => "Forms::Form"
   self.resource_class = Forms::Submission
-  
+
   manage_site_breadcrumb
   breadcrumb { [parent.label, parent] }
-  
+
   actions :show, :destroy, :update
-  
+
   respond_to :csv
-  
+
   def index
     respond_with(parent) do |format|
       format.csv { render :text => Forms::CsvExporter.new(parent).to_csv }
     end
   end
-  
+
   def update
     resource.spam!(params[:spam])
     respond_with(resource)
