a84e5f5910b5a59e767b8a7cb51d1483fca9c73a
Sam Pohlenz <sam@sampohlenz.com>
2019-05-31 14:53:48 +0930

    Basic reCAPTCHA v3 implementation

--- app/renderers/forms/form_renderer.rb
+++ app/renderers/forms/form_renderer.rb
@@ -3,28 +3,28 @@ class Forms::FormRenderer
 
   attr_accessor :output_buffer
   attr_reader :form, :submission, :template
-  
+
   def initialize(form, submission, template)
     @form, @submission, @template = form, submission, template
   end
-  
+
   def render
     form_tag(form.path) do
       concat form.fields.render(submission, template)
       concat default_submit_button unless has_submit_button?
     end
   end
-  
+
   def protect_against_forgery?
     false
   end
-  
+
 private
   def has_submit_button?
     form.fields.flatten.any? { |f| f.is_a?(Forms::Fields::SubmitButton) }
   end
-  
+
   def default_submit_button
-    content_tag(:div, submit_tag("Submit"), :class => "submit")
+    template.recaptcha + content_tag(:div, submit_tag("Submit"), :class => "submit")
   end
 end
--- app/renderers/forms/submit_button_renderer.rb
+++ app/renderers/forms/submit_button_renderer.rb
@@ -1,5 +1,5 @@
 class Forms::SubmitButtonRenderer < Forms::FieldRenderer
   def render
-    content_tag(:div, submit_tag(label), :class => "field submit")
+    template.recaptcha + content_tag(:div, submit_tag(label), :class => "field submit")
   end
 end
